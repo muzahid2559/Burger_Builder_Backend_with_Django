4176c352d6c53e3fb714d4a536c36913f9a5422f
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-11-26 21:34:01 +0600

    2 Creating Custom User Model - React with Django REST API

--- BurgerAPI/admin.py
+++ BurgerAPI/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from BurgerAPI.models import UserProfile, Profile
+
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Profile)
--- Burger_Builder_Backend/settings.py
+++ Burger_Builder_Backend/settings.py
@@ -30,6 +30,11 @@ INSTALLED_APPS = [
     "corsheaders"
 ]
 
+
+# Custom user model
+AUTH_USER_MODEL = 'BurgerAPI.UserProfile'
+
+
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
@@ -112,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
